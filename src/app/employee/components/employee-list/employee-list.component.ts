9d0832987c831b469fadd236febb671cf11223b9
cesar2064 <cesar0986@gmail.com>
2018-09-20 08:42:32 -0500

    Moved search form to employee list component

--- src/app/employee/components/employee-list/employee-list.component.ts
+++ src/app/employee/components/employee-list/employee-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ViewChild, Output, OnChanges } from '@angular/core';
 import { IEmployeeModel } from '../../../shared/models/employee.model';
 import { MatTableDataSource, MatSort } from '@angular/material';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { CommonComponent } from '../../../shared/abstract/common-component.abstract';
 import { RootStoreState } from '../../../root-store';
@@ -17,30 +17,33 @@ export class EmployeeListComponent extends CommonComponent implements OnInit {
 
   private employeesDataSource: MatTableDataSource<IEmployeeModel>;
 
+  searchForm: FormGroup;
   @Input()
   set employees(value: IEmployeeModel[]) {
     this.employeesDataSource = new MatTableDataSource(value);
     this.employeesDataSource.sort = this.sort;
   }
-
-  @Input() searchForm: {
-    group: FormGroup,
-    controlName: string
-  };
-
   @Input() isLoading: boolean;
 
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns: string[] = ['id', 'name', 'age', 'username', 'hireDate', 'actions'];
 
   constructor(
-    private store$: Store<RootStoreState.State>
+    private store$: Store<RootStoreState.State>,
+    private fb: FormBuilder
   ) {
     super();
   }
 
   ngOnInit() {
-    const searchControl = this.searchForm.group.controls[this.searchForm.controlName] as FormControl;
+
+    this.searchForm = this.fb.group(
+      {
+        valueToSearch: []
+      }
+    );
+
+    const searchControl = this.searchForm.controls.valueToSearch as FormControl;
 
     // search functionality
     searchControl.valueChanges.pipe(
--- src/app/employee/components/main/main.component.ts
+++ src/app/employee/components/main/main.component.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import { RootStoreState, EmployeeStoreSelectors, EmployeeStoreActions } from '../../../root-store';
 import { Observable } from 'rxjs';
 import { IEmployeeModel } from '../../../shared/models/employee.model';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 import { CommonComponent } from '../../../shared/abstract/common-component.abstract';
 
@@ -16,21 +15,15 @@ export class MainComponent extends CommonComponent implements OnInit {
   employees: IEmployeeModel[];
   error$: Observable<any>;
   isLoading$: Observable<boolean>;
-  searchForm: FormGroup;
 
   constructor(
-    private store$: Store<RootStoreState.State>,
-    private fb: FormBuilder
+    private store$: Store<RootStoreState.State>
   ) {
     super();
   }
 
   ngOnInit() {
-    this.searchForm = this.fb.group(
-      {
-        valueToSearch: []
-      }
-    );
+
     this.isLoading$ = this.store$.select(
       EmployeeStoreSelectors.selectEmployeeIsLoading
     ).pipe(takeUntil(this.destroy$));
